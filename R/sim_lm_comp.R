e9a86a27cb5d6f4637cc08b2be1840885f634981
Mackson Ncube <macksonncube@gmail.com>
2022-08-23 16:14:35 -0700

    Add parameter and if branch to run sim_lm_comp with no priord.

--- R/sim_lm_comp.R
+++ R/sim_lm_comp.R
@@ -2,6 +2,8 @@
 #'
 #' @param fit_obj A list of model fits built from the sim_lm_run function
 #'
+#' @param with_prior Set to 0 if data does not contain prior data.
+#'
 #' @return a nested list of performance metric data frames.  One top level list for the main data
 #' and one top level list for the prior data.  Within each top level list, there is one data frame of
 #' performance metrics for each model.
@@ -9,7 +11,7 @@
 #' @importFrom stats sd var
 #'
 #' @export
-sim_lm_comp <- function(fit_obj){
+sim_lm_comp <- function(fit_obj, with_prior = 1){
 
   #Create local bindings for variables
   `1` <- NULL
@@ -67,7 +69,7 @@ sim_lm_comp <- function(fit_obj){
         dplyr::slice_head(n = 1)
 
       #Connect model fit with model info
-      if (nrow(mod_infos[[j]]) == 1){
+      if (nrow(mod_infos[[j]]) == 1 | with_prior == 0){
         comp_main <- cbind(tidy_fit,
                            t(mod_infos[[j]][mod_infos[[j]]$prior == 0,]),
                            mod_name) %>%
@@ -124,39 +126,40 @@ sim_lm_comp <- function(fit_obj){
                         covered) %>%
           dplyr::distinct()
 
+        if (with_prior != 0){
 
-        res_prior <- do.call(rbind, res_prior)
+          res_prior <- do.call(rbind, res_prior)
 
-        res_prior <- res_prior %>% dplyr::mutate(parameter = as.numeric(parameter)) %>%
-          dplyr::mutate(covered = ifelse(parameter >= conf.low & parameter <= conf.high, 1, 0)) %>%
-          dplyr::group_by(term) %>%
-          dplyr::summarise(true_param = mean(parameter),
-                           K = dplyr::n(),
-                           t_bar = mean(estimate), # mean of estimates
-                           bias = t_bar - true_param, # bias
-                           var_t =  var(estimate), # variance
-                           s_t = sd(estimate), # standard deviation
-                           g_t = (1/(K * s_t^3)) * sum((estimate - t_bar)^3), # skewness
-                           k_t = (1/(K * s_t^4)) * sum((estimate - t_bar)^4), # kurtosis
-                           mse = mean((estimate - true_param)^2),  # calculate mse
-                           #jacknife
-                           t_bar_j = (1/(K - 1)) * (K * t_bar - estimate), # jacknife t bar
-                           bias_j_sq = (t_bar_j - true_param)^2, # jacknife bias
-                           s_sq_t_j = (1 / (K - 2)) * ((K - 1) * var_t - (K / (K - 1)) * (estimate - t_bar)^2), # jacknife var
-                           rmse_j = sqrt(bias_j_sq + s_sq_t_j), # jacknife rmse
-                           bias_mcse = sqrt(var_t / K),
-                           var_mcse = var_t * sqrt(((k_t - 1) / K)),
-                           mse_mcse = sqrt((1/K) * (s_t^4 * (k_t -1) + 4 * s_t^3 * g_t * bias + 4 * var_t * bias^2)),
-                           rmse = sqrt(mse),
-                           rmse_mcse = sqrt(((K - 1)/(K)) * sum((rmse_j - rmse)^2)),
-                           covered = mean(covered)) %>%
-          dplyr::ungroup() %>%
-          dplyr::select(term, parameter = true_param, iteration = K, bias, bias_mcse,
-                        var = var_t, var_mcse, mse, mse_mcse, rmse, rmse_mcse,
-                        covered) %>%
-          dplyr::distinct()
+          res_prior <- res_prior %>% dplyr::mutate(parameter = as.numeric(parameter)) %>%
+            dplyr::mutate(covered = ifelse(parameter >= conf.low & parameter <= conf.high, 1, 0)) %>%
+            dplyr::group_by(term) %>%
+            dplyr::summarise(true_param = mean(parameter),
+                             K = dplyr::n(),
+                             t_bar = mean(estimate), # mean of estimates
+                             bias = t_bar - true_param, # bias
+                             var_t =  var(estimate), # variance
+                             s_t = sd(estimate), # standard deviation
+                             g_t = (1/(K * s_t^3)) * sum((estimate - t_bar)^3), # skewness
+                             k_t = (1/(K * s_t^4)) * sum((estimate - t_bar)^4), # kurtosis
+                             mse = mean((estimate - true_param)^2),  # calculate mse
+                             #jacknife
+                             t_bar_j = (1/(K - 1)) * (K * t_bar - estimate), # jacknife t bar
+                             bias_j_sq = (t_bar_j - true_param)^2, # jacknife bias
+                             s_sq_t_j = (1 / (K - 2)) * ((K - 1) * var_t - (K / (K - 1)) * (estimate - t_bar)^2), # jacknife var
+                             rmse_j = sqrt(bias_j_sq + s_sq_t_j), # jacknife rmse
+                             bias_mcse = sqrt(var_t / K),
+                             var_mcse = var_t * sqrt(((k_t - 1) / K)),
+                             mse_mcse = sqrt((1/K) * (s_t^4 * (k_t -1) + 4 * s_t^3 * g_t * bias + 4 * var_t * bias^2)),
+                             rmse = sqrt(mse),
+                             rmse_mcse = sqrt(((K - 1)/(K)) * sum((rmse_j - rmse)^2)),
+                             covered = mean(covered)) %>%
+            dplyr::ungroup() %>%
+            dplyr::select(term, parameter = true_param, iteration = K, bias, bias_mcse,
+                          var = var_t, var_mcse, mse, mse_mcse, rmse, rmse_mcse,
+                          covered) %>%
+            dplyr::distinct()
+        }
       }
-
     }
 
     #Store across simulations
