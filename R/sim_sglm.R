a4850002774f56bf368d130d89f7504eeef1180d
Mackson Ncube <macksonncube@gmail.com>
2022-09-08 11:35:40 -0700

    added documentation for sim_sglm

--- R/sim_sglm.R
+++ R/sim_sglm.R
@@ -1,9 +1,27 @@
+#' Simulate Data Using simglm's sim_args Parameter
+#'
+#' @param data this parameter was taken from simglm:
+#' https://github.com/lebebr01/simglm/blob/main/R/fixef_sim.r
+#' "Data simulated from other functions to pass to this function.
+#' Can pass NULL if first in simulation string."
+#' @param sim_args Define model using simglm's sim_args parameter
+#' @param prior indicates whether df will serve as main data (0) or
+#' prior data (1)
+#' @param mod_name model alias
+#' @param ... Other arguments to pass to simglm's simulate_fixed,
+#' simulate_randomeffect, simulate_error, or generate_response
+#'
+#' @return A list with the dataframe produced by the model specification
+#' (as an object named df_mod) and the model specification information
+#' (as an object named df_infor)
+#' @export
 sim_sglm <- function(data = NULL,
                      sim_args,
                      prior = 0,
                      mod_name = "mod",
                      ...){
-  #Try to simulate data with random effects (in try block) or without (if try-error)
+  #Try to simulate data with random effects (in try block)
+  #or without (if try-error)
   df_mod <- try(simglm::simulate_fixed(data = NULL, sim_args) %>%
                   simglm::simulate_randomeffect(sim_args) %>%
                   simglm::simulate_error(sim_args) %>%
