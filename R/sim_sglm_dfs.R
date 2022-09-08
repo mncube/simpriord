bc18a56220ff248c00d2020416e65f7f89cb4df9
Mackson Ncube <macksonncube@gmail.com>
2022-09-08 13:45:02 -0700

    Initial simg_sglm_dfs code

---
+++ R/sim_sglm_dfs.R
@@ -0,0 +1,76 @@
+#' Simulate Datasets Using simglm's sim_args Parameter
+#'
+#' @param data_m this parameter was taken from simglm:
+#' https://github.com/lebebr01/simglm/blob/main/R/fixef_sim.r
+#' "Data simulated from other functions to pass to this function.
+#' Can pass NULL if first in simulation string." (main data)
+#' @param sim_args_m Define model using simglm's sim_args parameter (main data)
+#' @param data_p "Data simulated from other functions to pass to this function.
+#' Can pass NULL if first in simulation string." (previous data)
+#' @param sim_args_p Define model using simglm's sim_args parameter (previous data)
+#' @param with_prior if with_prior = 1, generate data frames that contain main and previous data
+#' @param frames The number of data frames to generate
+#' @param mod_name A list of model aliases
+#' @param ... Other arguments to pass to simglm's simulate_fixed,
+#' simulate_randomeffect, simulate_error, or generate_response
+#'
+#' @return A nested list of data frames (see sim_sglm for more information)
+#' @export
+sim_sglm_dfs <- function(data_m = NULL,
+                         sim_args_m,
+                         data_p = NULL,
+                         sim_args_p,
+                         with_prior = 1,
+                         frames = 2,
+                         mod_name = list("mod1"),
+                         ...) {
+  #Initialize lists to store dfs
+  all_df_mod <- vector(mode = "list", length = frames)
+  all_df_info <- vector(mode = "list", length = frames)
+
+  #Loop through and make dfs based on function parameters
+  for (i in 1:frames){
+
+    #Get mod name
+    if (length(mod_name) == 1){
+      mni <- unlist(mod_name)
+    } else {
+      mni <- mod_name[[i]]
+    }
+
+    #Get each df for main data
+    df <- sim_sglm(data = data_m,
+                   sim_args_m,
+                   prior = 0,
+                   mod_name = mni,
+                   ...)
+
+    #If prior data is included, get each df for prior data
+    if (with_prior == 1){
+      df_prior <- sim_sglm(data = data_p,
+                         sim_args_p,
+                         prior = 1,
+                         mod_name = mni,
+                         ...)
+
+      #Combine main and prior data into one df
+      df_mod <- rbind(df$df_mod, df_prior$df_mod)
+      df_info <- list("main" = df$df_info, "prior" = df_prior$df_info)
+    } else {
+      #Get main data as df_mod
+      df_mod <- df$df_mod
+      df_info <- df$df_info
+    }
+
+    #Collect dfs in lists
+    all_df_mod[[i]] <- df_mod
+    all_df_info[[i]] <- df_info
+  }
+
+  #Collect output
+  Output <- list("df_mod" = all_df_mod,
+                 "df_info" = all_df_info)
+
+  #Return dfs
+  return(Output)
+}
---
+++ tests/testthat/test-sim_sglm_dfs.R
@@ -0,0 +1,39 @@
+test_that("Test 1: sim_sglm_dfs returns an object which contains an object of class data.frame when run on
+sim_args using data similar to that simulated in the Generate Response Variable section of
+the Tidy Simulations vignette (Source:
+          https://cran.r-project.org/web/packages/simglm/vignettes/tidy_simulation.html)",
+          {
+            #Get main data
+            args_main <- list(
+              formula = y ~ 1 + weight + age + sex,
+              fixed = list(weight = list(var_type = 'continuous', mean = 0, sd = 1),
+                           age = list(var_type = 'ordinal', levels = 30:60),
+                           sex = list(var_type = 'factor', levels = c('male', 'female'))),
+              error = list(variance = 25),
+              sample_size = 10,
+              reg_weights = c(2, 0.3, -0.1, 0.5)
+            )
+
+            #Get prior data
+            args_prior <- list(
+              formula = y ~ 1 + weight + age + sex,
+              fixed = list(weight = list(var_type = 'continuous', mean = 0, sd = 2),
+                           age = list(var_type = 'ordinal', levels = 30:60),
+                           sex = list(var_type = 'factor', levels = c('male', 'female'))),
+              error = list(variance = 25),
+              sample_size = 10,
+              reg_weights = c(2, 0.4, -0.2, 0.5)
+            )
+
+            #Return data frames with info
+            grv_out <- sim_sglm_dfs(data_m = NULL,
+                                    sim_args_m = args_main,
+                                    data_p = NULL,
+                                    sim_args_p = args_prior,
+                                    with_prior = 1,
+                                    frames = 2,
+                                    mod_name = list("mod1"))
+            expect_s3_class(
+              grv_out$df_mod[[1]],
+              "data.frame")
+          })
